78292e2b17a7a7cd1e1de319b6e9a382030b2a42
Anatoxine <anais.andre@aol.fr>
2022-01-04 15:41:56 +0100

    daiuhziduz

--- Plateau.java
+++ Plateau.java
@@ -1,9 +1,11 @@
 public class Plateau {
     
-    private Case [][] grille = new Case [15] [15];
+    private Case [][] g = new Case [15] [15];
 
 
     public Plateau(){
+
+
         int [][] plateau ={
             {5,1,1,2,1,1,1,5,1,1,1,2,1,1,5},
             {1,4,1,1,1,3,1,1,1,3,1,1,1,4,1},
@@ -22,11 +24,38 @@ public class Plateau {
             {5,1,1,2,1,1,1,5,1,1,1,2,1,1,5}
         };
         
+        for(int i=0;i<15;i++){
+
+            for(int j=0;j<15;j++){
+                g[i][j]= new Case(plateau[i][j]);
+            }
+        
+        }
+    }
+    
+
+    public Plateau (Case[][] plateau) {
+        this.g = plateau;
     }
 
     public String toString(){
-        String res="_______________"+this;
-        
+        String res="_______________\n";
+        for(int i=0;i<15;i++){
+            res+="|";
+
+            for(int j=0;j<15;j++){
+
+                
+                if(!g[i][j].estRecouverte()&& g[i][j].getCouleur()!=1){
+                    res+= g[i][j].getCouleur();
+                }
+
+                
+
+            }
+
+            res+="|\n";
+        }
         
         return res;
     }
