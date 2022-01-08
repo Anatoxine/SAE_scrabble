72e48d3f61eb75f989d9754c5c8aa3ac999fd909
Tanguy Macé <tanguymace2002@gmail.com>
2022-01-08 22:17:40 +0100

    tests globaux

--- Scrabble.java
+++ Scrabble.java
@@ -11,7 +11,8 @@ public class Scrabble {
     private int numJoueur;
     private Plateau plateau;
     private MEE sac;
-    private static  int[] nbPointsJeton = {1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10};
+    
+    public static  int[] nbPointsJeton = {1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10};
 
     public Scrabble(Joueur[] joueurs) {
 
