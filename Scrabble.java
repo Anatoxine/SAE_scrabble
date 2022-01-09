3209cdacd835792eb5ab96c0323f5dc3eb0436f6
Anaïs <anais.andre@aol.fr>
2022-01-09 15:20:55 +0100

    en cours

--- Scrabble.java
+++ Scrabble.java
@@ -7,8 +7,7 @@ public class Scrabble {
         int []tab={3,2,5};
         MEE chevalet= new MEE (tab);
         Plateau plateau=new Plateau();
-        Ut.afficher(plateau.placementValide("AAA",7,7,'h',chevalet));
-        Ut.afficher(plateau.nbPointsPlacement("AAA", 1, 1, 'h', nbPointsJeton));
+       
 
     }
     
@@ -19,7 +18,7 @@ public class Scrabble {
     
     public static  int[] nbPointsJeton = {1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10};
 
-    public Scrabble(Joueur[] joueurs) {
+    public Scrabble(String[] joueurs) {
 
         this.joueurs = joueurs;
         this.numJoueur = Ut.randomMinMax(1, joueurs.length);
